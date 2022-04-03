ed5a00d6bf631a2aafc5c5b29485f4e5312229f3
Winston Peng <panda501wp@gmail.com>
2022-04-03 12:26:09 -0400

    Add initial project, and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# UB Rate My Professor
+
+An app that let's us flip our destiny. Usually teachers judge students, but with this app we get to judge them. Ha ha! May this app bring the judgement of an angry god upon their heads.
---
+++ UBRateMyProfessor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UBRateMyProfessor
+//
+//  Created by Winston Peng on 4/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
