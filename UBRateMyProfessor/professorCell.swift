ce811d60c82a453d22167893927ec41852fd6d46
JJMxr04 <joemaiar@buffalo.edu>
2022-04-13 19:25:59 -0400

    Professor list screen 3

---
+++ UBRateMyProfessor/professorCell.swift
@@ -0,0 +1,26 @@
+//
+//  professorCell.swift
+//  UBRateMyProfessor
+//
+//  Created by Joe Maiarana on 4/13/22.
+//
+
+import UIKit
+
+class professorCell: UITableViewCell {
+
+    @IBOutlet weak var professorName: UILabel!
+    @IBOutlet weak var overallrating: UILabel!
+    @IBOutlet weak var subjectsList: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
