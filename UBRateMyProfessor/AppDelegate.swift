786a93c32eb28557125e0b4e4a3be8326d74212e
Muhtasim Mushfiq <muhtasim01@gmail.com>
2022-04-13 19:00:44 -0400

    Login Screen Done

--- UBRateMyProfessor/AppDelegate.swift
+++ UBRateMyProfessor/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "5TUPlqB7ROq7d9mEJa6EUEI2YC2bGTJbm8zZQsUi" // <- UPDATE
+                    $0.clientKey = "XxCQhD6qTTp5sonxA04kWJ6KYiEmHpgKMYg9aQw8" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com/"
+            }
+            Parse.initialize(with: parseConfig)
+            
         return true
     }
 
