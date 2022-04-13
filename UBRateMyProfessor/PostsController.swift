bb3eb7ecc60f309773132f7e0217763e3f448bb8
JJMxr04 <joemaiar@buffalo.edu>
2022-04-12 21:48:46 -0400

    Started the Professors List screen

---
+++ UBRateMyProfessor/PostsController.swift
@@ -0,0 +1,29 @@
+//
+//  PostsController.swift
+//  UBRateMyProfessor
+//
+//  Created by Joe Maiarana on 4/12/22.
+//
+
+import UIKit
+
+class PostsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
